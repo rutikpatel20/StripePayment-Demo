3b7923a5767f2566d505895a380a98f38a515119
rutiksimform <rutik.p@simformsolutions.com>
2022-06-23 15:19:13 +0530

    products validation added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, :price, presence: true
+
+  # Convert object into name if we write direct product instead of product.name still it gives product name properly
+  def to_s
+    name
+  end
 end
