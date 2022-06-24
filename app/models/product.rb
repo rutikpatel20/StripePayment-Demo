3c14371621d30b501ab5834305d8925e03185665
rutiksimform <rutik.p@simformsolutions.com>
2022-06-24 19:08:07 +0530

    add stripe_product_id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,10 @@ class Product < ApplicationRecord
   def to_s
     name
   end
+
+  after_create do
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "inr")
+    update(stripe_product_id: product.id)
+  end
 end
