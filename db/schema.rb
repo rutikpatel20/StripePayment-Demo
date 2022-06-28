0ee88782b9a737256453f3518cca4134a452c9a9
rutiksimform <rutik.p@simformsolutions.com>
2022-06-28 17:17:08 +0530

    products can have different currency

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_24_134113) do
+ActiveRecord::Schema.define(version: 2022_06_28_113329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_06_24_134113) do
     t.integer "sales_count", default: 0, null: false
     t.string "stripe_product_id"
     t.string "stripe_price_id"
+    t.string "currency", default: "inr"
   end
 
   create_table "users", force: :cascade do |t|
